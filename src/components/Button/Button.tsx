85a0252cbf98966c4c3c5579cc184fe2beca4e24
Faye Xue <fxue89@gmail.com>
2018-12-26 00:14:26 +1100

    fix: Use glamor due to storybook @emotion/core version conflict, will roll back when storybook update in Jan

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,5 @@
-/** @jsx jsx */
 import * as React from "react";
-import { jsx, css } from "@emotion/core";
+import { css } from "glamor";
 
 export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {}
 
@@ -8,6 +7,8 @@ const buttonStyle = css({
   WebkitAppearance: "none"
 });
 
-const Button = (props: ButtonProps) => <button css={buttonStyle}>{props.children}</button>;
+const Button = (props: ButtonProps) => (
+  <button className={`${buttonStyle}`}>{props.children}</button>
+);
 
 export default Button;
