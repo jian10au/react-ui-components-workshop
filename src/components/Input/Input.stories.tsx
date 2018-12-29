d5b8bd4b0603866ec9af4f3a237992978e6f9756
Vladimir Zotov <azuresplash@gmail.com>
2018-12-29 21:07:02 +1100

    Added needlessly complex trick

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -5,11 +5,14 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Input from './Input';
 
+const StatefulInput = (props: { defaultValue: string, label: string }) => {
+  const [value, setValue] = React.useState(() => props.defaultValue);
+  // Make sure current `value` is updated to match a new `defaultValue`;
+  React.useMemo(() => setValue(props.defaultValue), [props.defaultValue]);
+  return <Input label={props.label} value={value} onChange={e => setValue(e.currentTarget.value)} />
+}
+
 storiesOf('ui', module)
   .addDecorator(withKnobs)
   .addDecorator(withInfo)
-  .add('Input', () => {
-    const defaultValue = text('value', 'James');
-    const [value, setValue] = React.useState(defaultValue);
-    return <Input label={text('label', 'First Name')} value={value} onChange={e => setValue(e.currentTarget.value)} />
-  });
+  .add('Input', () => <StatefulInput defaultValue={text('value', 'James')} label={text('label', 'First Name')} />);
